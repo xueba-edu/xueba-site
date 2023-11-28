e0d554814ba3a12bb072f375b5f950e2f358d74b
Eric <73317463+NycRat@users.noreply.github.com>
2023-11-28 10:05:44 -0800

    backend: query from db when request is sent + cors

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,4 @@
-use crate::models::user::User;
 use sqlx::mysql::MySqlPoolOptions;
-use rocket::serde::uuid::Uuid;
 
 #[macro_use]
 extern crate rocket;
@@ -18,32 +16,17 @@ async fn rocket() -> _ {
         .await
         .unwrap();
 
-    match sqlx::query_as!(
-        User,
-        "SELECT user_id AS `user_id: Uuid`, name_first, name_last, email FROM user"
-    )
-    .fetch_one(&pool)
-    .await
-    {
-        Ok(user) => println!("{user:?}"),
-        Err(err) => match err {
-            sqlx::Error::RowNotFound => {
-                sqlx::query!(
-                    "INSERT INTO user (user_id, name_first, name_last, email) VALUES (?, ?, ?, ?)",
-                    Uuid::new_v4(),
-                    "Richie",
-                    "Zhang",
-                    "rqzhang@gmail.com"
-                )
-                .execute(&pool)
-                .await
-                .unwrap();
-            }
-            _ => println!("{err:?}"),
-        },
-    }
+    // let allowed_origins = rocket_cors::AllowedOrigins::some_exact(&[""]);
 
-    rocket::build().mount(
+    let cors = rocket_cors::CorsOptions {
+        // allowed_origins,
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()
+    .unwrap();
+
+    rocket::build().manage(pool).attach(cors).mount(
         "/api",
         routes![
             routes::user::post_user_login,
--- backend/src/routes/user.rs
+++ backend/src/routes/user.rs
@@ -1,10 +1,9 @@
 use rocket::{
     http::Status,
-    serde::{
-        json::Json,
-        uuid::Uuid,
-    },
+    serde::{json::Json, uuid::Uuid},
+    State,
 };
+use sqlx::MySqlPool;
 
 use crate::models::user::User;
 
@@ -15,31 +14,61 @@ pub async fn post_user_login() -> Status {
 
 #[post("/user/signup")]
 pub async fn post_user_signup() -> Status {
+    // sqlx::query!(
+    //     "INSERT INTO user (user_id, name_first, name_last, email) VALUES (?, ?, ?, ?)",
+    //     Uuid::new_v4(),
+    //     "Richie",
+    //     "Zhang",
+    //     "rqzhang@gmail.com"
+    // )
+    // .execute(&pool)
+    // .await
+    // .unwrap();
     Status::Ok
 }
 
 #[get("/user/info?<user_id>")]
-pub async fn get_user_info(user_id: Uuid) -> Result<(Status, Json<User>), Status> {
-    Ok((
-        Status::Ok,
-        Json(User {
-            user_id,
-            name_first: "Richie".into(),
-            name_last: "Zhang".into(),
-            email: "richiezhang06@gmail.com".into(),
-        }),
-    ))
+pub async fn get_user_info(
+    user_id: Uuid,
+    state: &State<MySqlPool>,
+) -> Result<(Status, Json<User>), Status> {
+    match sqlx::query_as!(
+        User,
+        r#"SELECT user_id AS `user_id: Uuid`, name_first, name_last, email 
+        FROM user 
+        WHERE user_id = ?"#,
+        user_id
+    )
+    .fetch_one(state.inner())
+    .await
+    {
+        Ok(user) => Ok((Status::Ok, Json(user))),
+        Err(err) => match err {
+            sqlx::Error::RowNotFound => Err(Status::NotFound),
+            _ => Err(Status::InternalServerError),
+        },
+    }
 }
 
 #[get("/user/list?<school_id>")]
-pub async fn get_user_list(school_id: i32) -> Result<(Status, Json<Vec<Uuid>>), Status> {
-    Ok((
-        Status::Ok,
-        Json(vec![
-            Uuid::new_v4(),
-            Uuid::new_v4(),
-            Uuid::new_v4(),
-            Uuid::new_v4(),
-        ]),
-    ))
+pub async fn get_user_list(
+    school_id: i32,
+    state: &State<MySqlPool>,
+) -> Result<(Status, Json<Vec<Uuid>>), Status> {
+    match sqlx::query!(
+        r#"SELECT user_id AS `user_id: Uuid`
+        FROM user"#
+    )
+    .fetch_all(state.inner())
+    .await
+    {
+        Ok(users) => Ok((
+            Status::Ok,
+            Json(users.into_iter().map(|x| x.user_id).collect()),
+        )),
+        Err(err) => match err {
+            sqlx::Error::RowNotFound => Err(Status::NotFound),
+            _ => Err(Status::InternalServerError),
+        },
+    }
 }
