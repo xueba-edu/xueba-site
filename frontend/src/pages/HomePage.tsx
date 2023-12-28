7d08bc6a1997404969003e25f9af967932152cc8
Eric Xiao <73317463+NycRat@users.noreply.github.com>
2023-12-27 18:49:10 -0800

    frontend: homepage + stuff

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,29 @@
+import { createEffect, createSignal } from "solid-js";
 import Navbar from "../Navbar";
+import { fetchUserInfo } from "../ServerFunctions";
 
 function HomePage() {
+  const [user, setUser] = createSignal("");
+
+  // const [count, setCount] = createSignal(0);
+  // const [ready, setReady] = createSignal(false);
+
+  createEffect(async () => {
+    if (user() === "") {
+      const userInfo = await fetchUserInfo(
+        "e7e8ed68-67f4-4dcc-ba74-cade10e73b9d",
+      );
+      setUser((_) => {
+        return JSON.stringify(userInfo);
+      });
+    }
+  });
+
   return (
     <div>
       <Navbar />
-      <h1>welcome</h1>
+      <h1>wecuome</h1>
+      <p>{user()}</p>
     </div>
   );
 }
